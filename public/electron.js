8536a1ee0e489610537d32f48331d61c60340521
jfstrength <jfstrength@crimson.ua.edu>
2019-12-07 14:56:52 -0500

    Removed autoupdating

--- public/electron.js
+++ public/electron.js
@@ -1,17 +1,12 @@
-import { app as _app, BrowserWindow as _BrowserWindow } from "electron";
-const app = _app;
-const BrowserWindow = _BrowserWindow;
-import { join } from "path";
-import isDev from "electron-is-dev";
-import { autoUpdater } from 'electron-updater';
-
-autoUpdater.logger = require('electron-log')
-autoUpdater.logger.transports.file.level = "info"
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
 
 let mainWindow;
 
 function createWindow() {
-    autoUpdater.checkForUpdatesAndNotify()
     mainWindow = new BrowserWindow({ width: 900, height: 680 });
     mainWindow.webContents.openDevTools()
     mainWindow.loadURL(
@@ -35,16 +30,3 @@ if (mainWindow === null) {
 createWindow();
 }
 });
-
-autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
-  });
-
-autoUpdater.on('update-downloaded', () => {
-    mainWindow.webContents.send('update_downloaded');
-    autoUpdater.quitAndInstall();
-  });  
-
-  setInterval(() => {
-    autoUpdater.checkForUpdatesAndNotify()
-  }, 60 * 60000)
